a3410368588444421a97ce01d0ad4096e60f4b0c
Vitor Junior <vitorjunior@poli.ufrj.br>
2022-03-30 01:42:31 -0300

    Form and Link redirect
    Inputs (email, password), button, states(email, password, loading), navigate on submit and axios success, post login
    data, alert on axios error and link redirect.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,48 @@
+import { useState } from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import axios from 'axios';
 
 export default () => {
-    return (
-        <div>
-            Login
-        </div>
-    );
+
+    const [email, setEmail] = useState<string>();
+    const [password, setPassword] = useState<string>();
+    const [loading] = useState<boolean>(false);
+    const browse = useNavigate();
+
+    const login = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const URL = 'https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login';
+        const response = axios.post(URL, {
+            email,
+            password
+        });
+        response.then(() => browse('/hoje'));
+        response.catch(() => alert('houve um erro :('));
+    };
+
+    if(loading) {
+        return (null);
+    } else {
+        return (
+            <>
+                <form onSubmit={login}>
+                    <input required
+                           type="email" 
+                           value={email}
+                           placeholder="email" 
+                           onChange={e => setEmail(e.target.value)}/>
+                    <input required
+                           type="password"
+                           value={password} 
+                           placeholder="senha" 
+                           onChange={e => setPassword(e.target.value)}/>
+                    <button type="submit">Entrar</button>
+                </form>
+                <Link to='/cadastro'>
+                    Não tem uma conta? Cadastre-se!
+                </Link>
+            </>
+        );
+    }
+    
 }
