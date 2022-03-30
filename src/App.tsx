c42b059bda678ba2b2554dd30ab75092b8da4422
Vitor Junior <vitorjunior@poli.ufrj.br>
2022-03-30 00:43:34 -0300

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Habits from './pages/Habits';
+import History from './pages/History';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Today from './pages/Today';
+
 export default () => {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Login/>}/>
+                <Route path='/cadastro' element={<SignUp/>}/>
+                <Route path='/habitos' element={<Habits/>}/>
+                <Route path='/hoje' element={<Today/>}/>
+                <Route path='/historico' element={<History/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
+};
---
+++ src/pages/Today.tsx
@@ -0,0 +1,8 @@
+
+export default () => {
+    return (
+        <div>
+            Hoje
+        </div>
+    );
+}
