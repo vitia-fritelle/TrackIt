03e073ca919b33755874d6dc737f7f49f9f31f8b
Vitor Junior <vitorjunior@poli.ufrj.br>
2022-03-30 00:52:19 -0300

    General Styles
    reset.css, font lexend deca, @types/styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import styled from 'styled-components';
+
+const GeneralStyles = styled.div`
+    font-family: 'Lexend Deca', sans-serif;
+`;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <GeneralStyles>
+            <App />
+        </GeneralStyles>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
+
